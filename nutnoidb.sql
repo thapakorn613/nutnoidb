031989c0cc7fde1db55d576624bfdb1d10c8e0d7
thapakorn pheyporn <thapakorn613@gmail.com>
2019-04-26 05:42:50 +0700

    Update nutnoidb.sql

--- nutnoidb.sql
+++ nutnoidb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2019 at 09:18 PM
+-- Generation Time: Apr 26, 2019 at 12:41 AM
 -- Server version: 10.3.8-MariaDB
 -- PHP Version: 7.2.7
 
@@ -45,20 +45,22 @@ CREATE TABLE `project` (
   `project_name` varchar(255) NOT NULL,
   `teacher_id1` int(10) DEFAULT NULL,
   `teacher_id2` int(10) DEFAULT NULL,
-  `teacher_id3` int(10) DEFAULT NULL
+  `teacher_id3` int(10) DEFAULT NULL,
+  `send_email` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`id`, `project_name`, `teacher_id1`, `teacher_id2`, `teacher_id3`) VALUES
-(1, 'Banana Marathon', 1, 2, 3),
-(2, 'virtual reality human anatomy', 4, 5, 6),
-(3, 'Program to Develop Reading in Junior High School Students with Learning Disability', 5, 4, 1),
-(4, ' Fantasy Grand Arena', 5, 6, 1),
-(5, 'Learning and Development Application Through PSP', 5, 4, 6),
-(6, 'Service Queuing Application', 5, 3, 2);
+INSERT INTO `project` (`id`, `project_name`, `teacher_id1`, `teacher_id2`, `teacher_id3`, `send_email`) VALUES
+(1, 'Banana Marathon', 1, 2, 3, 1),
+(2, 'virtual reality human anatomy', 4, 5, 6, NULL),
+(3, 'Program to Develop Reading in Junior High School Students with Learning Disability', 5, 4, 1, NULL),
+(4, ' Fantasy Grand Arena', 5, 6, 1, NULL),
+(5, 'Learning and Development Application Through PSP', 5, 4, 6, NULL),
+(6, 'Service Queuing Application', 5, 3, 2, NULL),
+(7, 'NutNoi', 1, 6, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -69,23 +71,23 @@ INSERT INTO `project` (`id`, `project_name`, `teacher_id1`, `teacher_id2`, `teac
 CREATE TABLE `teacher` (
   `id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `teamup` varchar(255) NOT NULL,
-  `googlecalendar` varchar(255) NOT NULL
+  `googlecalendar` varchar(255) NOT NULL,
+  `count2` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher` (`id`, `name`, `surname`, `email`, `teamup`, `googlecalendar`) VALUES
-(1, 'lachana', '', 'lachana@cmu.com', 'lachana@teamup', 'lachana@gmail.com'),
-(2, 'sakgasit', '', 'sakgasit@cmu.com', 'sakgasit@teamup', 'sakgasit@gmail.com'),
-(3, 'narissara', '', 'narissara@cmu.com', 'narissara@teamup', 'narissara@gmail.com'),
-(4, 'narathip', '', 'narathip@cmu.com', 'narathip@teamup', 'narathip@gmail.com'),
-(5, 'juggapong', '', 'juggapong@cmu.com', 'juggapong@teamup', 'juggapong@gmail.com'),
-(6, 'patiwet', '', 'patiwet@cmu.com', 'patiwet@teamup', 'patiwet@gmail.com');
+INSERT INTO `teacher` (`id`, `name`, `email`, `teamup`, `googlecalendar`, `count2`) VALUES
+(1, 'lachana', 'lachana@cmu.com', 'https://teamup.com/ksd952dt39h2ar9gxy', 'lachana@gmail.com', 0),
+(2, 'sakgasit', 'sakgasit@cmu.com', 'https://teamup.com/ksq7w7hu9f4c8697tm', 'sakgasit@gmail.com', 0),
+(3, 'narissara', 'narissara@cmu.com', 'https://teamup.com/ksfyc2gdssiofnmzy8', 'narissara@gmail.com', 0),
+(4, 'narathip', 'narathip@cmu.com', 'https://teamup.com/ksfyc2gdssiofnmzy8', 'narathip@gmail.com', 0),
+(5, 'juggapong', 'juggapong@cmu.com', 'https://teamup.com/ksq7w7hu9f4c8697tm', 'juggapong@gmail.com', 0),
+(6, 'patiwet', 'patiwet@cmu.com', 'https://teamup.com/ksd952dt39h2ar9gxy', 'patiwet@gmail.com', 0);
 
 -- --------------------------------------------------------
 
@@ -145,9 +147,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `booking_id`, `haveWaitTable`, `email`, `password`, `project_id`, `type`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'ภูมิ', 4, 1, 'chawalidpunyajarern@gmail.com', '$2y$10$axiLsVMoK/FiW.TGXmU9M.kaeTLTtaonAaD6y.DxpY92yqZBWl.wy', 2, NULL, NULL, '2019-03-19 10:40:45', '2019-03-19 10:40:45'),
-(2, 'art', NULL, 1, 'art@gmail.com', '$2y$10$/v.UBoB4qeWd7ho8QNSCX.zwfJHgT4dlzE52UZrSUGCV9id2Or6PK', NULL, 'admin', 'LOzfsrpI1kkthEvnxXZ0Ght4QGtOlneLMIVtlaEOB4RbuPHAZro8gbe2vGjb', '2019-03-21 02:56:25', '2019-03-27 02:44:42'),
-(3, 'thapakorn', NULL, 1, 'thapakorn@gmail.com', '$2y$10$6HWcUrQp9aFq3yfAwFhgWuU/PXIo8rgQ94M1WBPzSzQDXWidlMly2', 1, NULL, 'lyEPjd2FWDdXoDOflfGswrnubCr0dcGqgZEpsgt443nSfUSwwB6RzFlKDrrz', '2019-03-21 11:11:15', '2019-03-27 02:44:17'),
-(4, 'green', 9, 1, 'green@gmail.com', '$2y$10$UuAuOlmNKjgxzTD6MG1SlOEEivNe6OYq1FvndMW8Sn6FOjEFRpCyG', 3, NULL, 'i15mVwoyGmRo1fDCWDbetLrjpvME0xlkdjpVaUzrZldeWundhw4ggzAKLb9d', '2019-03-27 01:54:53', '2019-03-27 01:54:53');
+(2, 'art', NULL, 1, 'art@gmail.com', '$2y$10$/v.UBoB4qeWd7ho8QNSCX.zwfJHgT4dlzE52UZrSUGCV9id2Or6PK', NULL, 'admin', 'm7Jxc6vcUvjrAJd48q2hXRACWLmCtHALJiYMDGzZYnfKkFurS2eZSLIJZaPY', '2019-03-21 02:56:25', '2019-03-27 02:44:42'),
+(3, 'thapakorn', 3, 1, 'thapakorn@gmail.com', '$2y$10$6HWcUrQp9aFq3yfAwFhgWuU/PXIo8rgQ94M1WBPzSzQDXWidlMly2', 1, NULL, 'lyEPjd2FWDdXoDOflfGswrnubCr0dcGqgZEpsgt443nSfUSwwB6RzFlKDrrz', '2019-03-21 11:11:15', '2019-03-27 02:44:17'),
+(4, 'green', NULL, 1, 'green@gmail.com', '$2y$10$UuAuOlmNKjgxzTD6MG1SlOEEivNe6OYq1FvndMW8Sn6FOjEFRpCyG', 3, NULL, 'BQKiNCDxODUUMhZDsa9w5UzIN0xkPWEnTuHg4816khLKTWXWmA8hJfZwCDye', '2019-03-27 01:54:53', '2019-03-27 01:54:53'),
+(6, 'kan', NULL, NULL, 'kan@gmail.com', '$2y$10$MYQWBJF3QJrrudlxda1lzuadR2/mGF6lpLuMhrsRNawHaAQsBoPvi', NULL, NULL, 'efiw1qAqHUEu6dH7bVfJmJfcuRKak1XTVyCkZKFpQJGSlSj2elviPT6Rg7yS', '2019-04-25 14:50:15', '2019-04-25 14:50:15');
 
 -- --------------------------------------------------------
 
@@ -170,9 +173,9 @@ CREATE TABLE `waitconfirm` (
 --
 
 INSERT INTO `waitconfirm` (`wait_id`, `status_confirm`, `status_submit`, `project_id`, `booking_id1`, `booking_id2`, `booking_id3`) VALUES
-(4, NULL, 1, 1, 3, 4, 13),
-(5, 1, 1, 2, 2, 4, 10),
-(11, 1, 1, 3, 9, NULL, NULL);
+(4, 1, 1, 1, 3, 4, 13),
+(5, 1, 1, 2, 2, 4, NULL),
+(33, NULL, 1, 3, 9, 10, 11);
 
 --
 -- Indexes for dumped tables
@@ -238,7 +241,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `teacher`
@@ -256,13 +259,13 @@ ALTER TABLE `timebooking`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `waitconfirm`
 --
 ALTER TABLE `waitconfirm`
-  MODIFY `wait_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `wait_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
